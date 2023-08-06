b40a8749e9b209bff413c0584879dd21c6473613
ShravaniMishra <shravniguddi@gmail.com>
2023-08-07 00:08:42 +0530

    left part

--- src/components/LeftPart.jsx
+++ src/components/LeftPart.jsx
@@ -3,12 +3,20 @@ import React from 'react'
 const LeftPart = () => {
   return (
     <>
-    <div>
-      <h1></h1>
-      <h2></h2>
-    </div>
+      <div>
+        <div>
+          <h1>Alex-Powered Solutions,</h1>
+          <h2>Potential Unleashed</h2>
+
+          <p>
+            Discover your $100M offer with our AI-powered tools. Inspired by
+            Alex Hormozi's 100M Offers, we help startups and businesses alike
+            revolutionize their content and branding strategies.
+          </p>
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
 export default LeftPart
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,31 +1,30 @@
-import React from 'react';
+import React from "react";
 import styled from "styled-components";
 
 const Navbar = () => {
   return (
     <Wrapper>
-       <img src="https://i.ibb.co/zN2mdfd/Group-2.png" alt="logo." />   
+      <img src="https://i.ibb.co/zN2mdfd/Group-2.png" alt="logo." />
       <h3>Sign in</h3>
     </Wrapper>
   );
-}
+};
 
 const Wrapper = styled.div`
   width: 100%;
   height: auto;
+  padding: 15px 15px;
   display: flex;
   justify-content: space-between;
   line-height: normal;
   box-shadow: rgba(0, 0, 0, 0.3) 0px 19px 38px,
     rgba(0, 0, 0, 0.22) 0px 15px 12px;
   img {
-    padding-top: 20px;
     margin-left: 40px;
     width: 20%;
   }
 
   h3 {
-    padding-top: 20px;
     margin-right: 30px;
     font-weight: 600;
     color: #0176cc;
@@ -36,16 +35,15 @@ const Wrapper = styled.div`
     width: 100%;
     height: auto;
     display: flex;
+    padding: 20px 20px;
     justify-content: space-between;
     line-height: normal;
     img {
-      padding-top: 20px;
       margin-left: 40px;
       width: 40%;
     }
 
     h3 {
-      padding-top: 20px;
       margin-right: 30px;
       font-weight: 600;
       color: #0176cc;
@@ -58,5 +56,4 @@ const Wrapper = styled.div`
   } */
 `;
 
-
-export default Navbar
+export default Navbar;
