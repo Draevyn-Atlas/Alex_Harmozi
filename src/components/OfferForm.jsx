76337e9bd0e01b2241873b88c06e6ce4577649e6
Govind Kumar <gk4051668@gmail.com>
2023-08-07 01:17:25 +0530

    adde code for right part

---
+++ src/components/OfferForm.jsx
@@ -0,0 +1,78 @@
+import {
+  FormControl,
+  FormHelperText,
+  FormLabel,
+  VStack,
+  Radio,
+  RadioGroup,
+  Input,
+} from "@chakra-ui/react";
+import React from "react";
+import { styled } from "styled-components";
+
+const OfferForm = () => {
+  return (
+    <Offer>
+      <div className="offer_section_heading">
+        <h1>$100M Offers</h1>
+      </div>
+      <div className="offer_section_subheading">
+        <h3>From idea to Grand Slam Offer</h3>
+      </div>
+      <form>
+        <div className="form_control form_control_1">
+          <label htmlFor="">
+            Are you going to IMPROVE your clients health/wealth/relationships?
+          </label>
+          <div className="radio_group">
+            <Radio colorScheme="" value="Health">Health</Radio>
+            <Radio colorScheme="" value="Wealth">Wealth</Radio>
+            <Radio colorScheme="" value="Relationships">Relationships</Radio>
+          </div>
+        </div>
+        <div className="form_control form_control_2">
+          <label htmlFor="">
+            What is your target audience within this market? (e.g. entrepreneurs
+            looking to scale their business)
+          </label>
+          <Input placeContent={"Enter response"} />
+        </div>
+      </form>
+    </Offer>
+  );
+};
+const Offer = styled.div`
+  background: white;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  div.offer_section_heading {
+    text-align: center;
+  }
+
+  div.offer_section_heading h1 {
+    font-size: 1.5rem;
+    font-weight: 600;
+  }
+  div.offer_section_subheading {
+    text-align: center;
+  }
+  div.offer_section_subheading h3 {
+    color: #7e7e7e;
+  }
+
+  form {
+    width: 90%;
+
+    .form_control {
+      width: 100%;
+    }
+
+    .form_control_1 label {
+      width: 100%;
+    }
+  }
+`;
+
+export default OfferForm;
--- src/components/RightPart.jsx
+++ src/components/RightPart.jsx
@@ -1,7 +1,26 @@
 import React from "react";
+import { styled } from "styled-components";
+import OfferForm from "./OfferForm";
 
 const RightPart = () => {
-  return <div>RightPart</div>;
+  return (
+    <Right>
+      <div className="right_section_part1"></div>
+      <div className="right_section_part2"></div>
+      <OfferForm />
+    </Right>
+  );
 };
+const Right = styled.div`
+  background: #ddf0ff;
+  display: flex;
+  flex-direction: column;
+  padding: 1rem;
+  div {
+    /* border: 1px solid black; */
+    width: 85%;
+    align-self: center;
+  }
+`;
 
 export default RightPart;
